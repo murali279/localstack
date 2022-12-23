c27e2f3010afd9f0a220e4e77bb407c1977d1a62
Murali <muralip.wk@gmail.com>
2022-12-23 16:06:48 +0000

    Create dynamodb table

--- main.tf
+++ main.tf
@@ -9,6 +9,9 @@ provider "aws" {
   s3_force_path_style         = true
   endpoints {
     s3 = "http://localhost:4566"
+    dynamodb = "http://localhost:4566"
+    lambda   = "http://localhost:4566"
+    kinesis  = "http://localhost:4566"
   }
 }
 
@@ -17,6 +20,20 @@ resource "aws_s3_bucket" "b" {
   bucket = "onexlab-bucket-terraform"
   acl    = "public-read"
 }
+
+resource "aws_dynamodb_table" "dogs" {
+  name           = "dogs"
+  read_capacity  = "20"
+  write_capacity = "20"
+  hash_key       = "ID"
+
+  attribute {
+    name = "ID"
+    type = "S"
+  }
+}
+
+
 
 # Upload an object
 resource "aws_s3_bucket_object" "object" {
